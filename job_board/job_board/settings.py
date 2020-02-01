1a3ba1bb5d396bc9e01128b6f5fa557e5336e3ea
henry_sanford <hsanford97@gmail.com>
2020-02-01 12:21:18 -0500

    Update settings.py

--- job_board/job_board/settings.py
+++ job_board/job_board/settings.py
@@ -133,6 +133,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #emial settings
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wsanford99@gmail.com'
-EMAIL_HOST_PASSWORD = 'Longhaul99!'
+EMAIL_HOST_USER = 'magnusonvalidationemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'WebsitePass321'
 EMAIL_PORT = 587
